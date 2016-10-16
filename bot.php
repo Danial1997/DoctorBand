2e8e6d07931f770fc1cdeffc9c39c8b1c5101b39
Danial1997 <mynumberone245@gmail.com>
2016-10-16 06:15:01 -0900

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,13 +1,19 @@
 <?php
 
-$telegram = json_decode(
-    file_get_contents("https://api.telegram.org/bot113422787:AAGmxRD5ABzfGlEqMm4f0WzwavrHQeMCiNw/getMe")
-);
-
-if($telegram->ok == true){
-    echo "<h1>اطلاعات ربات</h1>";
-    echo "<b>نام کاربری </b>: ".$telegram->result->username;
-    echo "<br/><b> نام ربات </b>: ".$telegram->result->first_name;
-}else{
-    echo "مشکلی پیش آمد";
+function makeCurl($api,$method,$datas=[]){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"https://api.telegram.org/bot{$api}/{$method}");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+        http_build_query($datas));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $server_output = curl_exec ($ch);
+    curl_close ($ch);
+    return $server_output;
 }
+
+$api = "113422787:AAGmxRD5ABzfGlEqMm4f0WzwavrHQeMCiNw";
+$robot = makeCurl($api,"getMe");
+$updates = makeCurl($api,"getUpdates",[
+    "limit"=>1
+]);
