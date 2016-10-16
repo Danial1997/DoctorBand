2d56fd4cd69b6b7a5c8f60b1819b5ef40eb7173b
Danial1997 <mynumberone245@gmail.com>
2016-10-16 06:20:19 -0900

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-
+set_time_limit(0);
 function makeCurl($api,$method,$datas=[]){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,"https://api.telegram.org/bot{$api}/{$method}");
@@ -12,8 +12,29 @@ function makeCurl($api,$method,$datas=[]){
     return $server_output;
 }
 
-$api = "284890957:AAHovpM5UlIoV8eBhowl4KEZukFNjhQ9XQo";
-$robot = makeCurl($api,"getMe");
-$updates = makeCurl($api,"getUpdates",[
-    "limit"=>1
-]);
+$last_updated_id=0;
+function getUpdates(){
+    global $last_updated_id;
+    $updates = json_decode(
+        makeCurl("113422787:AAGmxRD5ABzfGlEqMm4f0WzwavrHQeMCiNw","getUpdates",[
+            "offset"=>($last_updated_id+1)
+        ])
+    );
+    if($updates->ok == true && count($updates->result) > 0){
+        foreach($updates->result as $update){
+            $last_updated_id = $update->update_id;
+            $chat_id = $update->message->chat->id;
+            $message_id = $update->message->message_id;
+            $text= "پیام شما رسید .";
+            makeCurl("284890957:AAHovpM5UlIoV8eBhowl4KEZukFNjhQ9XQo","sendMessage",[
+               "chat_id"=>$chat_id,
+                "reply_to_message_id"=>$message_id,
+                "text"=>$text
+            ]);
+        }
+    }
+
+    sleep(5);
+    getUpdates();
+}
+getUpdates();
